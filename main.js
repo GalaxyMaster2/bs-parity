9aac2d9cb41220e1e584a82f6a7494420b01b9ba
GalaxyMaster2 <10052298+GalaxyMaster2@users.noreply.github.com>
2020-06-08 13:29:39 +0200

    initial bomb reset support

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const cutDirections = ['up', 'down', 'left', 'right', 'upLeft', 'upRight', 'downLeft', 'downRight', 'dot'];
-const types = ['red', 'blue', 'bomb'];
+// bombs are type 3 for some reason
+const types = {
+    0: 'red',
+    1: 'blue',
+    3: 'bomb'
+};
+const lineIndices = ['left', 'middleLeft', 'middleRight', 'right'];
+const lineLayers = ['bottom', 'middle', 'top'];
+
+// the minimum time between the last note or bomb for a bomb to be considered for each saber
+// make user configurable?
+const bombMinTime = 0.25;
 
 const cuts = {
     blue: {
@@ -45,17 +56,26 @@ class Parity {
             }
         }
 
-        if (firstRed && cuts.red.good.forehand.indexOf(cutDirections[firstRed._cutDirection]) != -1) {
+        if (firstRed && cuts.red.good.forehand.includes(cutDirections[firstRed._cutDirection])) {
             this.red = 'forehand';
         } else {
             this.red = 'backhand';
         }
 
-        if (firstBlue && cuts.blue.good.forehand.indexOf(cutDirections[firstBlue._cutDirection]) != -1) {
+        if (firstBlue && cuts.blue.good.forehand.includes(cutDirections[firstBlue._cutDirection])) {
             this.blue = 'forehand';
         } else {
             this.blue = 'backhand';
         }
+    }
+
+    setRed(x) {
+        console.log('red parity set to ' + x);
+        this.red = x;
+    }
+    setBlue(x) {
+        console.log('blue parity set to ' + x);
+        this.blue = x;
     }
 }
 
@@ -103,8 +123,17 @@ function getNotes(obj) {
 }
 
 function logNote(note, parity) {
-    console.log('Note at beat ' + note._time + ' -- ' + types[note._type] + ' -- ' +
-        cutDirections[note._cutDirection] + ' -- ' + parity);
+    let time = note._time;
+    let type = types[note._type];
+    let cutDirection = cutDirections[note._cutDirection];
+    let column = lineIndices[note._lineIndex];
+    let row = lineLayers[note._lineLayer];
+
+    if (type === 'bomb') {
+        console.log('Bomb at beat ' + time + ' -- ' + column + ' -- ' + row + ' -- red saber: ' + parity.red + ', blue saber: ' + parity.blue);
+    } else {
+        console.log('Note at beat ' + time + ' -- ' + type + ' -- ' + cutDirection + ' -- ' + column + ' -- ' + row + ' -- ' + parity[type]);
+    }
 }
 
 function main() {
@@ -122,32 +151,80 @@ function main() {
         let note = notes[i];
         let type = types[note._type];
         let cutDirection = cutDirections[note._cutDirection];
+        let column = lineIndices[note._lineIndex];
+        let row = lineLayers[note._lineLayer];
 
-        // skip bombs for now
         if (type === 'bomb') {
-            continue;
-        }
-
-        if (cuts[type].good[parity[type]].indexOf(cutDirection) != -1) {
-            parity.invert(type);
-        } else if (cuts[type].borderline[parity[type]].indexOf(cutDirection) != -1) {
-            logNote(note, parity[type]);
-            console.log('borderline');
-            parity.invert(type);
-        } else {
-            logNote(note, parity[type]);
-            console.log('bad, assuming wrist reset');
-        }
-
-        // invert parity again if there's a same-color note within sliderPrecision
-        let offset = 1;
-        while (((i + offset) < notes.length) &&
-            (parseFloat((notes[i + offset]._time - notes[i]._time).toFixed(10)) <= parseFloat((1 / sliderPrecision).toFixed(10)))) {
-            if (notes[i]._type === notes[i + offset]._type) {
-                parity.invert(type);
-                break;
+            // this is super ugly, I'm hoping to come up with a better way later
+            if (!(['middleLeft', 'middleRight'].includes(column)) || !(['bottom', 'top'].includes(row))) {
+                continue;
             }
-            offset++;
+
+            let setRed = true;
+            let setBlue = true;
+            let offset = -1;
+            let offsetNote = notes[i + offset];
+            while (((i + offset) >= 0) &&
+                (parseFloat((note._time - offsetNote._time).toFixed(10)) <= parseFloat(bombMinTime.toFixed(10)))) {
+                switch (types[offsetNote._type]) {
+                    case 'bomb':
+                        if (lineIndices[offsetNote._lineIndex] === 'middleLeft') {
+                            setRed = false;
+                        } else if (lineIndices[offsetNote._lineIndex] === 'middleRight') {
+                            setBlue = false;
+                        }
+                        break;
+                    case 'red':
+                        setRed = false;
+                        break;
+                    case 'blue':
+                        setBlue = false;
+                        break;
+                }
+                offset--;
+                offsetNote = notes[i + offset];
+            }
+
+            logNote(note, parity);
+            if (row === 'bottom') {
+                if (setRed) {
+                    parity.setRed('forehand');
+                }
+                if (setBlue) {
+                    parity.setBlue('forehand');
+                }
+            } else if (row === 'top') {
+                if (setRed) {
+                    parity.setRed('backhand');
+                }
+                if (setBlue) {
+                    parity.setBlue('backhand');
+                }
+            }
+        } else {
+            if (cuts[type].good[parity[type]].includes(cutDirection)) {
+                parity.invert(type);
+            } else if (cuts[type].borderline[parity[type]].includes(cutDirection)) {
+                logNote(note, parity);
+                console.log('borderline');
+                parity.invert(type);
+            } else {
+                logNote(note, parity);
+                console.log('bad, assuming wrist reset');
+            }
+
+            // invert parity again if there's a same-color note within sliderPrecision
+            let offset = 1;
+            let offsetNote = notes[i + offset];
+            while (((i + offset) < notes.length) &&
+                (parseFloat((offsetNote._time - note._time).toFixed(10)) <= parseFloat((1 / sliderPrecision).toFixed(10)))) {
+                if (note._type === offsetNote._type) {
+                    parity.invert(type);
+                    break;
+                }
+                offset++;
+                offsetNote = notes[i + offset];
+            }
         }
     }
 }
