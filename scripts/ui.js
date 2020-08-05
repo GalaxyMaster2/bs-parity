9691a38e35c67bac14554c5207129d3a6ab26b07
nathan <11426516+infinityiowastaken@users.noreply.github.com>
2020-08-05 16:45:55 +0100

    bugfix: error loading dat files due to offset grabbing

--- scripts/ui.js
+++ scripts/ui.js
@@ -441,7 +441,7 @@ function loadMapInfo(datString) {
 
 /**
  * gets the local time offset of the map and converts it to beats
- * @param {Object} datString - the parsed contents of a difficulty.dat file
+ * @param {Object} songInfo - the parsed contents of a difficulty.dat file
  */
 function getLocalOffset(songInfo = getSelectedDiff()) {
     try {
