a0a080f10a8155f39af42b50b06a37c3758791d3
nathan <11426516+infinityiowastaken@users.noreply.github.com>
2020-08-29 20:38:27 +0100

    why not

--- scripts/ui.js
+++ scripts/ui.js
@@ -332,3 +332,126 @@ const easterEggTitles = [
 function randomizeTitle() {
     pageTitle.textContent = easterEggTitles[Math.floor(Math.random() * easterEggTitles.length)];
 }
+
+var a = 30, b = 70;
+var canvasDots = function(dotnumber) {
+    var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d'),
+    colorDot = '#dddddd',
+    color = '#aaaaaa';
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.fillStyle = colorDot;
+    ctx.lineWidth = .1;
+    ctx.strokeStyle = color;
+
+    var mousePosition = {
+        x: 30 * canvas.width / 100,
+        y: 30 * canvas.height / 100
+    };
+
+    var dots = {
+        nb: dotnumber,
+        distance: 150,
+        d_radius: 100,
+        array: []
+    };
+
+    function Dot(){
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+
+        this.vx = -.5 + Math.random();
+        this.vy = -.5 + Math.random();
+
+        this.radius = Math.random();
+    }
+
+    Dot.prototype = {
+        create: function(){
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            ctx.fill();
+        },
+
+        animate: function(){
+            for(i = 0; i < dots.nb; i++){
+                var dot = dots.array[i];
+
+                if(dot.y < 0 || dot.y > canvas.height){
+                    dot.vx = dot.vx;
+                    dot.vy = - dot.vy;
+                }
+                else if(dot.x < 0 || dot.x > canvas.width){
+                    dot.vx = - dot.vx;
+                    dot.vy = dot.vy;
+                }
+                if ((dot.x - mousePosition.x) < b && (dot.y - mousePosition.y) < b && (dot.x - mousePosition.x) > - b && (dot.y - mousePosition.y) > - b) {
+                    dot.vx *= 1.05;
+                    dot.vy *= 1.05;
+                } else if (Math.abs(dot.vx) > 0.5 || Math.abs(dot.vy) > 0.5) {
+                    dot.vx *= 0.98;
+                    dot.vy *= 0.98;
+                }
+                dot.x += dot.vx;
+                dot.y += dot.vy;
+            }
+        },
+
+        line: function(){
+            for(i = 0; i < dots.nb; i++){
+                for(j = 0; j < dots.nb; j++){
+                    i_dot = dots.array[i];
+                    j_dot = dots.array[j];
+
+                    if((i_dot.x - j_dot.x) < a && (i_dot.y - j_dot.y) < a && (i_dot.x - j_dot.x) > - a && (i_dot.y - j_dot.y) > - a){
+                        // if((i_dot.x - mousePosition.x) < b && (i_dot.y - mousePosition.y) < b && (i_dot.x - mousePosition.x) > - b && (i_dot.y - mousePosition.y) > - b){
+                            ctx.beginPath();
+                            ctx.moveTo(i_dot.x, i_dot.y);
+                            ctx.lineTo(j_dot.x, j_dot.y);
+                            ctx.stroke();
+                            ctx.closePath();
+                        // }
+                    }
+                }
+            }
+        }
+    };
+
+    function createDots(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for(i = 0; i < dots.nb; i++){
+            dots.array.push(new Dot());
+            dot = dots.array[i];
+
+            dot.create();
+        }
+
+        dot.line();
+        dot.animate();
+    }
+
+    window.onmousemove = function(parameter) {
+        mousePosition.x = parameter.pageX;
+        mousePosition.y = parameter.pageY;
+    }
+
+    window.onresize = function() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+
+    mousePosition.x = window.innerWidth / 2;
+    mousePosition.y = window.innerHeight / 2;
+
+    function dotrender() {
+        if (!introDiv.classList.contains('done')) {
+            window.requestAnimationFrame(dotrender);
+            createDots();
+        } else {
+            return;
+        }
+    }
+    dotrender();
+};
+canvasDots(500);
