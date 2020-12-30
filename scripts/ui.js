65038442e511bf88da4f102fd1058724278dd200
Thomas Cheyney <top_cat@thomasc.co.uk>
2020-12-30 15:28:59 +0000

    Better url regex

--- scripts/ui.js
+++ scripts/ui.js
@@ -317,7 +317,7 @@ function clearProgress() {
  */
 function validateUrl(url) {
     const validurl = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((([a-z\\d]|[a-z\\d][a-z\\d-]*[a-z\\d])\\.)*([a-z\\d]|[a-z\\d][a-z\\d-]*[a-z\\d])|' + // domain name
         '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
         '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
         '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
